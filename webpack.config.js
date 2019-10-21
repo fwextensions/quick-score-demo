868bff00ffee5e8f21a0835767b8ef3acb531569
John Dunning <fw@johndunning.com>
2019-10-20 17:56:42 -0700

    Update to latest packages
    Squashed commit of the following:
    
    commit 88804726d203bf69886e3bf8822c1291479570d3
    Author: John Dunning <fw@johndunning.com>
    Date:   Sun Oct 20 17:29:20 2019 -0700
    
        Use plain input instead of Input component
    
        Managing the cursor position seems to no longer be an issue.
        Remove Input.js.
    
    commit abd4cd97b20f25cc4af3631f6c8a002864ab17d6
    Author: John Dunning <fw@johndunning.com>
    Date:   Sun Oct 20 16:55:13 2019 -0700
    
        Update to latest packages
    
        Update to Babel 7.
        Fix vulnerabilities in lodash, eslint-utils, set-value and mixin-deep.
        Switch to getDerivedStateFromProps() instead of deprecated componentWillReceiveProps() in Input.
        Change innerRef to ref for styled components.
        CleanWebpackPlugin no longer takes a param.

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,16 @@
-const path = require("path");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 //const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const {getIfUtils} = require("webpack-config-utils");
+const {getIfUtils, removeEmpty} = require("webpack-config-utils");
 
 
-module.exports = (env) => {
-	const {ifProduction} = getIfUtils(env);
+module.exports = (env, argv) => {
+	const {ifProduction} = getIfUtils(argv.mode);
 
 	return {
-		devtool: ifProduction("source-map", "eval-source-map"),
-		entry: "./src/index.js",
-		resolve: {
-			alias: {
-				"@": path.resolve(__dirname, "src"),
-			},
-		},
-		output: {
-			path: __dirname + "/dist",
-			filename: "bundle.js"
-		},
-		devServer: {
-			static: {
-				directory: "./dist"
-			}
-		},
-		stats: {
-				// log the build time in the console
-			builtAt: true
-		},
+		devtool: ifProduction() ? "source-map" : "eval-sourcemap",
+		entry: "./src/js/index.js",
 		module: {
 			rules: [
 				{
@@ -43,19 +24,24 @@ module.exports = (env) => {
 				}
 			]
 		},
+		resolve: {
+			extensions: ["*", ".js", ".jsx"]
+		},
+		output: {
+			path: __dirname + "/dist",
+			filename: "bundle.js"
+		},
+		devServer: {
+			contentBase: "./dist"
+		},
 		plugins: [
 			new CleanWebpackPlugin(),
-			new CopyWebpackPlugin({
-				patterns: [
-					{ from: "./src/css/", to: "css" }
-				]
-			}),
+			new CopyWebpackPlugin([
+				{ from: "./src/css/", to: "css" }
+			]),
 			new HtmlWebpackPlugin({
 				template: "./src/index.html",
-				filename: "index.html",
-				minify: false,
-					// add a timestamp that's injected into an HTML comment
-				buildTime: new Date().toISOString()
+				filename: "index.html"
 			})
 		]
 	}
