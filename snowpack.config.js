ec69dfe9e80ce333539f2e35c154a96d838f7013
John Dunning <fw@johndunning.com>
2022-02-09 14:27:31 -0800

    Add snowpack
    Add @snowpack/plugin-babel package to parse JSX in .js files.
    Add start:snowpack script.
    Add index.js module into index.html, since it doesn't get automatically injected.

---
+++ snowpack.config.js
@@ -0,0 +1,21 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    src: "/"
+  },
+  plugins: [
+    "@snowpack/plugin-babel"
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
